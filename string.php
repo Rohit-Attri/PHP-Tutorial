663beda589664f514acc6bd9fe16312b19830556
Rohit Attri <110565520+Rohit-Attri@users.noreply.github.com>
2023-07-22 23:15:21 +0530

    Add files via upload

--- string.php
+++ string.php
@@ -1,21 +1,68 @@
 <?php
-$value = "this is a string. let's check it's functions";
-echo strlen($value);
-echo "<br/>";
-echo strtoupper($value);
-echo "<br/>";
-echo strtolower($value);
-echo "<br/>";
-echo str_word_count($value);
-echo "<br/>";
-echo strrev($value);
-echo "<br/>";
-echo trim($value,"Ts");//it removes the specified character or white spaces from the string from both sides 
-echo "<br/>";
-echo ucfirst($value);
-echo "<br/>";
-echo ucwords($value);
+$name = "Rohit is good boy";
+echo "$name";
+echo "<br>";
+echo "This String have " . strlen($name) . "  Characters";
+echo "<br>";
+echo "The reverse of  String is " . strrev($name);
+
+// dot (.) operator is use to concatinate the strings in php 
+echo "<br>";
+echo "The Postion of is " . strpos($name, "is");
+echo "<br>";
+echo "Rohit is replaced here as " . str_replace("Rohit", "Mohit", $name);
+echo "<br>";
+$driver_age = 45;
+if ($driver_age > 25 && $driver_age < 65) {
+    echo "You can drive";
+} else {
+    echo "You can not Drive!";
+}
+echo "<br>";
+
+// $star = "*";
+// for($i = 1; $i<=5; $i++){
+
+//     for($j = 1; $j<=$i ; $j++){
+//         echo($star);
+//     }
+//     echo ("<br>");
+// }
+
+// $star = "*";
+// for ($i = 1; $i <= 5; $i++) {
+
+//     for ($k = 1; $k <= 5 - $i; $k++) {
+//         echo ("&nbsp;&nbsp;");
+//     }
+
+//     for ($j = 1; $j <= $i; $j++) {
+//         echo ($star);
+//     }
+//     echo ("<br>");
+// }
 
 
+// the below code is for triangle pattern
+$star = "*";
+$x=20;
+for ($i = 1; $i <= $x; $i++) {
+
+    for ($k = 1; $k <= $x - $i; $k++) {
+        echo ("&nbsp;&nbsp;");
+    }
+
+    for ($j = 1; $j <= $i; $j++) {
+        echo ($star);
+    }
+
+    for ($l = 2; $l <= $i; $l++) {
+        echo ("*");
+    }
+    echo ("<br>");
+}
+
+echo("<br/>");
+echo(date("jS, F , Y, g:i:s "));
 
 ?>
